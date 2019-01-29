5934ad968abf1b76f20029d2e8990d19b95432cb
Jing <jtan1993@gmail.com>
2019-01-29 05:38:48 +0000

    set up button prefab

---
+++ Assets/Scripts/ButtonSetup.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSetup : MonoBehaviour
+{
+
+    public Button buttonComponent;
+    public Text nameLabel;
+    public Image iconImage;
+    public Text priceText;
+
+    void Start()
+    {
+
+    }
+
+    public void Setup()
+    {
+
+
+    }
+
+    public void HandleClick()
+    {
+
+    }
+}
